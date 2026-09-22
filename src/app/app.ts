dc25bb45019391d6eb24b6abdde56d1d0bf53441
Laura Blanchard <blanchardlaura5@gmail.com>
2026-09-22 12:20:07 +0200

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./footer/footer";
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
