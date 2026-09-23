fbb6a9fe49b20226b19282043dc644094a2c40d8
Laura Blanchard <blanchardlaura5@gmail.com>
2026-09-23 10:49:50 +0200

    body

---
+++ src/app/body/body.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-body',
+  styleUrl: './body.scss',
+  templateUrl: './body.html',
+})
+export class Body {}
