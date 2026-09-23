a34b94c4d69810701944e70c2e788d62e8671811
Laura Blanchard <blanchardlaura5@gmail.com>
2026-09-23 10:17:08 +0200

    footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   imports: [],
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
 })
 export class Footer {
-
+  @Input() footerColor = 'orange';
 }
